39d4b921455b28e052fa0888e17e0b1e6915288d
rajan1602 <121995595+rajan1602@users.noreply.github.com>
2024-03-26 09:27:24 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)  we are using nodejs , not browser
+
+console.log(3+3);// code readability should be high 
+console.log("Rajan");
+
+let name = "rajan"
+let age  = 19
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); // object
