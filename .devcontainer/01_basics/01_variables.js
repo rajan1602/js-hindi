3d1688577b2726467cd586a53eb6d99b5497089d
rajan1602 <121995595+rajan1602@users.noreply.github.com>
2024-03-26 09:49:03 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "rajan@google.com"
+var accountPassword = "Rajan@12"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2 not allowed
+
+accountEmail = "ch@ch.com"
+accountPassword = "2121212121"
+accountCity = "Rajasthan"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
