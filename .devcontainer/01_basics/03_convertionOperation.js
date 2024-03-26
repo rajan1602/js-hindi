9c4d3dd7217a008ca79b187808c77b563ffa22b0
rajan1602 <121995595+rajan1602@users.noreply.github.com>
2024-03-26 09:48:08 +0000

    convertion of data type

---
+++ .devcontainer/01_basics/03_convertionOperation.js
@@ -0,0 +1,26 @@
+let score = "rajan"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+isloggedIn = "rajan"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rajan" => true
+let someNumber = 43
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
